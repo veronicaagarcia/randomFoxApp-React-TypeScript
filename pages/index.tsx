8d87e6d2fc8c5cb3aa21878f1e05f88a8db547d5
Veronica Garcia <veroagarcia90@gmail.com>
2023-10-06 10:35:23 -0300

    State con tipos primitivos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { RandomFox } from './compnents/RandomFox'
@@ -6,6 +7,12 @@ import styles from '../styles/Home.module.css'
 const random = () => Math.floor(Math.random() * 123) + 1;
 
 const Home: NextPage = () => {
+
+  const [images, setImages] = useState([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`
+  ])
   
   return (
     <div className={styles.container}>
@@ -17,8 +24,13 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Random Fox Images - React y TypeScript
         </h1>
-        <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`}/>
-  
+        <p className={styles.footer}> Un componente generico de React para cargar imagenes con Lazy Loading</p>
+        <p className={styles.footer}> Las imagenes agregadas no se descargaran hasta que sean visibles en la pantalla</p>
+        {images.map((image, index) => (
+          <div key={index} className='p-4'>
+            <RandomFox image={image}/>
+          </div>
+        ))}
       </main>
 
       <footer className={styles.footer}>
