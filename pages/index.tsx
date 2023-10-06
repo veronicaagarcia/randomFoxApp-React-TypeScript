fc6d44002dd94b860ed5c5bd7c5a495e65d4be02
Veronica Garcia <veroagarcia90@gmail.com>
2023-10-06 11:19:55 -0300

    Tipos para eventos y callbacks de escuchadores

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { MouseEventHandler, useState } from 'react';
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { RandomFox } from './compnents/RandomFox'
@@ -6,17 +6,20 @@ import styles from '../styles/Home.module.css'
 import { type } from 'os';
 
 const random = () => Math.floor(Math.random() * 123) + 1;
-type ImageItem = Array<{id:string, url:string}>;
+type ImageItem = {id:string, url:string};
 const generatedId = () => Math.random().toString(36).substr(2,9);
 
 const Home: NextPage = () => {
 
-  const [images, setImages] = useState<ImageItem>([
-    {id: generatedId() , url:`https://randomfox.ca/images/${random()}.jpg`},
-    {id: generatedId() , url:`https://randomfox.ca/images/${random()}.jpg`},
-    {id: generatedId() , url:`https://randomfox.ca/images/${random()}.jpg`},
-  ])
+  const [images, setImages] = useState<Array<ImageItem>>([])
      
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault()
+    const newImage: ImageItem =  {id: generatedId(), url:`https://randomfox.ca/images/${random()}.jpg`};
+
+    setImages([...images, newImage])
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -29,6 +32,8 @@ const Home: NextPage = () => {
         </h1>
         <p className={styles.footer}> Un componente generico de React para cargar imagenes con Lazy Loading</p>
         <p className={styles.footer}> Las imagenes agregadas no se descargaran hasta que sean visibles en la pantalla</p>
+
+        <button onClick={addNewFox} className="bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-full">Add new Fox Image</button>
         {images.map(({id, url}) => (
           <div key={id} className='p-4'>
             <RandomFox image={url}/>
